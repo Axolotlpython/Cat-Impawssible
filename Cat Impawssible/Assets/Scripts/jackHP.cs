a3553a3170d05979d0133ce1808e0ec7002d2fa4
Axolotlpython <Axolotl413@gmail.com>
2026-05-29 13:27:57 -0700

    +Art and HP

---
+++ Cat Impawssible/Assets/Scripts/jackHP.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jackHP : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HP healthbar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthbar.SetHealth(currentHealth);
+
+        if (currentHealth < 0)
+        {
+            GameManager.Instance.LoseGame();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        TakeDamage(1);
+    }
+
+}
