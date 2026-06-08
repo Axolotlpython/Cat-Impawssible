2d137980f9e5040e7ad948d22f38da753c4dc06f
Axolotlpython <Axolotl413@gmail.com>
2026-06-08 12:08:21 -0700

    UI Update

--- Cat Impawssible/Assets/Scripts/Menu Manager.cs
+++ Cat Impawssible/Assets/Scripts/Menu Manager.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
     public GameObject optionsPanel;
+    public GameObject creditspanel;
+    public GameObject Pausepenel;
+    public GameObject helppanel;
 
     public void PlayGame()
     {
@@ -17,6 +20,29 @@ public class MenuManager : MonoBehaviour
         SceneManager.LoadScene("Title");
     }
 
+    public void OpenHelp()
+    {
+        helppanel.SetActive(true);
+    }
+
+    public void CloseHelp()
+    {
+        helppanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Pausepenel.SetActive(true);
+        }
+    }
+
+    public void BackToGame()
+    {
+        Pausepenel.SetActive(false);
+    }
+
     public void OpenOptions()
     {
         optionsPanel.SetActive(true);
@@ -25,6 +51,16 @@ public class MenuManager : MonoBehaviour
     public void CloseOptions()
     {
         optionsPanel.SetActive(false);
+    }
+
+    public void OpenCredits()
+    {
+        creditspanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditspanel.SetActive(false);
     }
 
     public void QuitGame()
